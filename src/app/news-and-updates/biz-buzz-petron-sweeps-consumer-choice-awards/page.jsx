56d90a0058a9bf09d91df32e6ff3b2564fff1260
Jonathan Llano <jonathan.llano@ph.c3-interactive.com>
2024-03-06 22:12:47 +0800

    change ' to &apos;

--- src/app/news-and-updates/biz-buzz-petron-sweeps-consumer-choice-awards/page.jsx
+++ src/app/news-and-updates/biz-buzz-petron-sweeps-consumer-choice-awards/page.jsx
@@ -9,9 +9,9 @@ export default function Page() {
         <br /><br />
         Every year, international brand research firm Standard Insights ranks the best products and services according to market perception in their Consumer Choice Awards.
         <br /><br />
-        And in the petroleum sector, the awards for the retail oil and gas segment are closely watched. And this year, Filipino consumers spoke with unanimity on which petroleum firm is the country's best.
+        And in the petroleum sector, the awards for the retail oil and gas segment are closely watched. And this year, Filipino consumers spoke with unanimity on which petroleum firm is the country&apos;s best.
         <br /><br />
-        According to Standard Insights, Petron is the best in the country with a 45.7-percent rating, beating Pilipinas Shell's 31 percent.
+        According to Standard Insights, Petron is the best in the country with a 45.7-percent rating, beating Pilipinas Shell&apos;s 31 percent.
         <br /><br />
         “Petron has secured the title of best overall retail oil and gas brand in the Philippines by a resounding margin, cementing its position as the preferred choice of consumers,” the firm said.
         <br /><br />
@@ -21,7 +21,7 @@ export default function Page() {
         <br /><br />
         Note, of course, that this rating comes even if Petron diligently pays its taxes, compared to other fuel retailers who can offer lower pump prices by dodging government import levies.
         <br /><br />
-        In terms of accessibility, Standard Insights said Petron was the runaway winner with a rating of 53.6 percent versus Shell's 27.3 percent – naturally because the petroleum unit of San Miguel Corp. has the biggest gas station network in the country.
+        In terms of accessibility, Standard Insights said Petron was the runaway winner with a rating of 53.6 percent versus Shell&apos;s 27.3 percent – naturally because the petroleum unit of San Miguel Corp. has the biggest gas station network in the country.
         <br /><br />
         Petron also emerged on top in terms of sustainability and environmental friendliness; and branding and marketing.
         <br /><br />
